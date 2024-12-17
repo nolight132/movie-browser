2672cde432559915ec29fe6d0860e214e3fde784
nolight132 <olizkopavel@proton.me>
2024-12-17 16:42:36 +0100

    Don't optimize images

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,9 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ["image.tmdb.org"],
+    domains: ['image.tmdb.org'],
+    unoptimized: true,
   },
 };
 
