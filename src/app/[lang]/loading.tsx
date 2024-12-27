5610f9f215af1778d3d5ab53c02083cf41a9683d
nolight132 <olizkopavel@proton.me>
2024-12-27 16:15:26 +0100

    Fix loading

--- src/app/[lang]/loading.tsx
+++ src/app/[lang]/loading.tsx
@@ -3,7 +3,7 @@ import { LoadingSpinner } from '@/components/ui/spinner';
 const ListLoading = ({ className }: { className?: string }) => {
   return (
     <div
-      className={`w-full h-96 flex justify-center items-center ${className}`}
+      className={`w-full h-screen flex justify-center items-center ${className}`}
     >
       <LoadingSpinner className="size-8" />
     </div>
